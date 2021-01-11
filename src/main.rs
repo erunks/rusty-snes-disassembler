39375bbbde0a234b1b1d24a831144e9ce1859366
Edward Runkel <eddierunkel@gmail.com>
2021-01-11 17:00:47 -0500

    Add functionality to get the file path from the user, not just hardcoded

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ use std::process;
 mod opcode;
 mod disassembler;
 
+fn get_input() -> String {
+    use std::io;
+    println!("Enter path to ROM file:");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {},
+        Err(_) => {},
+    }
+    input.trim().to_string()
+}
+
 fn main() {
     let mut d = disassembler::Disassembler::new();
-    d.load_rom("./roms/Pieces.smc");
+    d.load_rom(&get_input());
     if let Err(err) = d.disassemble_rom() {
         println!("{}", err);
         process::exit(1);
